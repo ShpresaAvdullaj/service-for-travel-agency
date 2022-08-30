9c93bd1e60702ce0238fa7a4c47403f51764dd7c
ShpresaAvdullaj <99510468+ShpresaAvdullaj@users.noreply.github.com>
2022-08-30 00:23:12 -0700

    update dropdown button administrator with some opsions in location menu which includes places for location, also add CR,create retrive for these options

--- administrator/urls.py
+++ administrator/urls.py
@@ -4,13 +4,32 @@ from . import views
 urlpatterns = [
     path("administrator/trips/", views.get_trips_list, name="trips-list"),
     path("administrator/trips/add/", views.add_trip, name="add-trip"),
+    path("administrator/location/", views.index, name="location"),
     path(
-        "administrator/continents/", views.get_continents_list, name="continents-list"
+        "administrator/location/continents", views.get_continents_list, name="continents-list"
     ),
-    path("administrator/continents/add/", views.add_continent, name="continent-add"),
+    path("administrator/location/continents/add/", views.add_continent, name="continent-add"),
     path(
-        "administrator/continents/<int:pk>/",
+        "administrator/location/continents/<int:pk>/",
         views.get_continent_detail,
         name="continent-detail",
+    ),
+    path(
+        "administrator/location/countries/", views.get_countries_list, name="countries-list"
+    ),
+    path("administrator/location/countries/add", views.add_country, name="country-add"),
+    path(
+        "administrator/location/countries/<int:pk>/",
+        views.get_country_detail,
+        name="country-detail",
+    ),
+    path(
+        "administrator/location/cities/", views.get_cities_list, name="cities-list"
+    ),
+    path("administrator/location/cities/add", views.add_city, name="city-add"),
+    path(
+        "administrator/location/cities/<int:pk>/",
+        views.get_city_detail,
+        name="city-detail",
     ),
 ]
--- administrator/views.py
+++ administrator/views.py
@@ -1,18 +1,40 @@
 from django.shortcuts import render
 
-from administrator.forms import ContinentForm, TripForm
-from .models import Continent, Trip
+from administrator.forms import CityForm, ContinentForm, CountryForm, TripForm
+from .models import City, Continent, Country, Trip
 from django.shortcuts import redirect, get_object_or_404
 
 from django.views.generic import CreateView
+
+
+def index(request):
+    return render(request, "administrator/index_location.html")
 
 
 def get_continent_detail(request, pk):
     continent = get_object_or_404(Continent, pk=pk)
     return render(
         request,
-        "administrator/continents/continent_detail.html",
+        "administrator/location/continent_detail.html",
         context={"continent": continent},
+    )
+
+
+def get_country_detail(request, pk):
+    country = get_object_or_404(Country, pk=pk)
+    return render(
+        request,
+        "administrator/location/country_detail.html",
+        context={"country": country},
+    )
+
+
+def get_city_detail(request, pk):
+    city = get_object_or_404(City, pk=pk)
+    return render(
+        request,
+        "administrator/location/city_detail.html",
+        context={"city": city},
     )
 
 
@@ -27,11 +49,33 @@ def get_trips_list(request):
     )
 
 
+def get_countries_list(request):
+    countries = Country.objects.all()
+    return render(
+        request,
+        "administrator/location/countries_list.html",
+        context={
+            "countries": countries,
+        },
+    )
+
+
+def get_cities_list(request):
+    cities = City.objects.all()
+    return render(
+        request,
+        "administrator/location/cities_list.html",
+        context={
+            "cities": cities,
+        },
+    )
+
+
 def get_continents_list(request):
     continents = Continent.objects.all()
     return render(
         request,
-        "administrator/continents/continents_list.html",
+        "administrator/location/continents_list.html",
         context={
             "continents": continents,
         },
@@ -58,7 +102,33 @@ def add_continent(request):
     else:
         form = ContinentForm()
     return render(
-        request, "administrator/continents/continent_add.html", context={"form": form}
+        request, "administrator/location/continent_add.html", context={"form": form}
+    )
+
+
+def add_country(request):
+    if request.method == "POST":
+        form = CountryForm(request.POST, request.FILES)
+        if form.is_valid():
+            country = form.save()
+            return redirect("country-detail", pk=country.pk)
+    else:
+        form = CountryForm()
+    return render(
+        request, "administrator/location/country_add.html", context={"form": form}
+    )
+
+
+def add_city(request):
+    if request.method == "POST":
+        form = CityForm(request.POST, request.FILES)
+        if form.is_valid():
+            city = form.save()
+            return redirect("city-detail", pk=city.pk)
+    else:
+        form = CityForm()
+    return render(
+        request, "administrator/location/city_add.html", context={"form": form}
     )
 
 
