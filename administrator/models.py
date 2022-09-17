71dbbc171141771572842c16d7dfc9bbfeed3777
ShpresaAvdullaj <99510468+ShpresaAvdullaj@users.noreply.github.com>
2022-09-17 11:20:29 -0700

    remove descrption from hotels

--- administrator/models.py
+++ administrator/models.py
@@ -61,7 +61,7 @@ class Hotel(models.Model):
 
     def __str__(self):
         return (
-            f"{self.name}-{self.standart}-{self.description}-{self.photo}-{self.city}"
+            f"{self.name}-{self.standart}-{self.photo}-{self.city}"
         )
 
     def get_absolute_url(self):
--- core/settings.py
+++ core/settings.py
@@ -120,13 +120,10 @@ USE_TZ = True
 
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "assets"
-#STATICFILES_DIRS = [BASE_DIR / "assets"]
+STATIC_ROOT = BASE_DIR / "static"
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static/"),
-)
+STATICFILES_DIRS = [BASE_DIR / "assets"]
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
